da3ad6b762a4979e5ed2196bb08adbbb5ef50997
DrJupiter <klausjupiter@hotmail.com>
2024-02-14 10:56:09 +0100

    Notes week 3

--- Exercises/week3/time3.py
+++ Exercises/week3/time3.py
@@ -1,19 +1,25 @@
 import numpy as np
-from time import perf_counter as time
+from time import perf_counter as timecounter
+import plotly.graph_objects as go
 import wandb
 # initialize wandb
 wandb.init(project="Python High Performance Computing 2024", entity="ai-dtu", name="Measuring Performance")
-
-SIZES = np.logspace(1,4.5,base=10)
-
+SIZES = np.logspace(1,4.5,base=10, dtype=int)
+column_times = []
+row_times = []
 for step, SIZE in enumerate(SIZES):
-    SIZE = int(SIZE)
-    wandb.log({"SIZE": SIZE, "Step": step}, step=step)
     mat = np.random.rand(SIZE, SIZE)
-    t = time()
+    t = timecounter()
     double_column = 2 * mat[:,0]
-    t1 = time()
+    t1 = timecounter()
     double_row = 2 * mat[0,:]
-    t2 = time()
-    wandb.log({"Column Time": t1-t, "Row Time": t2-t1})
-    #print(f"SIZE {SIZE}: Column time {t1-t}, Row time {t2-t1}")
+    t2 = timecounter()
+    column_times.append(t1-t)
+    row_times.append(t2-t1)
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=SIZES, y=column_times, mode='lines+markers', name='Column'))
+fig.add_trace(go.Scatter(x=SIZES, y=row_times, mode='lines+markers', name='Row'))
+fig.update_layout(title='Column vs Row Vectorization', xaxis_title='Matrix Size', yaxis_title='Time (s)')
+wandb.log({"Column vs Row Vectorization": fig})
+wandb.finish()
